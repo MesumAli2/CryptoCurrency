be5ca25d7f14956b02624bd9251bce11fb169d22
MesumAli2 <smesumali20@gmail.com>
2023-04-15 20:04:03 +0400

    navigation back stack

--- app/src/main/java/com/mesum/cryptoproject/ui/main/MainActivity.kt
+++ app/src/main/java/com/mesum/cryptoproject/ui/main/MainActivity.kt
@@ -9,6 +9,11 @@ import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.mesum.bitcoinapp.viewmodel.CoinViewModel
 import com.mesum.cryptoproject.R
@@ -22,10 +27,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         window.statusBarColor = resources.getColor(R.color.gray)
-
         val actionBar = supportActionBar
         val colorDrawable = ColorDrawable(Color.parseColor("#1F2630"))
         actionBar?.setBackgroundDrawable(colorDrawable)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
